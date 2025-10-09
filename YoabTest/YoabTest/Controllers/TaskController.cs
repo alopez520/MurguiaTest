45c7d91124fcd9e98c70a054a0591d07399b1693
Yoab <kevin02lopez02@gmail.com>
2025-10-08 20:14:17 -0600

    Se crea el controlador para las tareas (TaskController)

---
+++ YoabTest/YoabTest/Controllers/TaskController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using YoabTest.Models;
+using YoabTest.Services;
+
+namespace YoabTest.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly TaskServices _taskService;
+
+        public TaskController()
+        {
+            _taskService = new TaskServices();
+        }
+
+        public IActionResult Index()
+        {
+            var task = _taskService.GetAll();
+            return View(task);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskService.Add(task);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            var task = _taskService.GetAll().FirstOrDefault(
+                t => t.Id == id);
+            if (task ==null)
+                return NotFound();
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult ToggleComplete(int id)
+        {
+            _taskService.ToggleComplete(id);
+            return Json(new { success = true });
+        }
+
+        [HttpPost]
+        public IActionResult Edit(TaskModel task)
+        {
+            if (ModelState.IsValid)
+            {
+                _taskService.Update(task);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(task);
+        }
+
+        [HttpPost]
+        public IActionResult ToggleImportant(int id)
+        {
+            var task = _taskService.GetById(id);
+            if (task == null) return Json(new { success = false });
+
+            task.IsImportant = !task.IsImportant;
+            _taskService.Update(task);
+
+            return Json(new { success = true });
+        }
+
+        public IActionResult Delete (int id)
+        {
+            var task = _taskService.GetAll().FirstOrDefault(
+                t => t.Id == id);
+            return View(task);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _taskService.Delete(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
--- YoabTest/YoabTest/obj/Debug/net8.0/YoabTest.AssemblyInfo.cs
+++ YoabTest/YoabTest/obj/Debug/net8.0/YoabTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+496e7acfb472c729ff532d78ec00d1b84d5fb975")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61d24b1d386ad526b10bb5c926a50d900b279e19")]
 [assembly: System.Reflection.AssemblyProductAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
