29add466cc76858c5f81677aa08c43d807822c0c
Yoab <kevin02lopez02@gmail.com>
2025-10-09 19:54:33 -0600

    Se agrega el respectivo script para el boton de editar en la misma pagina.

--- YoabTest/YoabTest/obj/Debug/net8.0/YoabTest.AssemblyInfo.cs
+++ YoabTest/YoabTest/obj/Debug/net8.0/YoabTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ae1a9211a9187469df7af33b615ded9cd108d0eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ce911682061a08434a7c0d68940e4168dae3807")]
 [assembly: System.Reflection.AssemblyProductAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("YoabTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ YoabTest/YoabTest/wwwroot/js/script.js
@@ -0,0 +1,14 @@
+﻿function editTask(id) {
+    const button = document.querySelector(`button[onclick='editTask(${id})']`);
+    if (!button)
+        return;
+
+    const row = button.closest("tr");
+    if (!row)
+        return;
+
+    document.querySelector('#taskForm input[name="Id"]').value = id;
+    document.querySelector('#taskForm input[name="Title"]').value = row.cells[0].innerText.trim();
+    document.querySelector('#taskForm textarea[name="Description"]').value = row.cells[1].innerText;
+    document.querySelector('#taskForm button[type="submit"]').textContent = "Actualizar";
+}
